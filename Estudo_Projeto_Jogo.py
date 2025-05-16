527ed8cf85ea15464cf0e4f508de8810b154114b
Anderson Araujo <aoliveira832@gamil.com>
2025-05-16 14:06:07 -0300

    atualizando projeto

--- Estudo_Projeto_Jogo.py
+++ Estudo_Projeto_Jogo.py
@@ -5,19 +5,40 @@ import random
 from os import system, name
 
 #Função para limpar a tela a cada execução
-
 def limpa_tela():
-	#Windows
-	if name == 'nt':
-	   	   _= system('cls')
-	
-	#Mac ou Linux
-	else:
-		_= system('clear')
+ 
+    # Windows
+    if name == 'nt':
+        _ = system('cls')
+ 
+    # Mac ou Linux
+    else:
+        _ = system('clear')
+        
 
 
-print('Olá Vscode')
-	
+#Função 
+
+def game():
+    
+	limpa_tela()     
+	print("\nBem-vindo(a) ao jogo dda forca!")
+	print("Adivinhe a palavra abaixo:\n")
+
+# Lista de palvras para o jogo
+palavras = ['banana', 'abacate', 'uva', 'morango', 'laranja']
+
+# Escolhe randomicamente uma palvra
+palvra = random.choice(palavras)
+
+# List comprehensio
+letras_descobertas = ['_' for letra in palvra]
+
+# Número de chances
+chances = 6
+
+# Lista para as letras erradas
+letras_erradas = []
 
 
 
