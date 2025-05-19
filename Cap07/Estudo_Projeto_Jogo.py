61b24cf413f06f4bc05065f25968a6fbc8fcb9de
Anderson Araujo <aoliveira832@gamil.com>
2025-05-19 11:58:25 -0300

    atualizando projeto de estudo

--- Cap07/Estudo_Projeto_Jogo.py
+++ Cap07/Estudo_Projeto_Jogo.py
@@ -1,38 +1,40 @@
-#Projeto 1 - Desenvolvimento de Game em Python V1
+# Projeto 1 - Desenvolvimento de Game em Python V1
 
-#Import
-import random 
+# Import
+import random
 from os import system, name
 
-#Função para limpar a tela a cada execução
+
+# Função para limpar a tela a cada execução
 def limpa_tela():
- 
+
     # Windows
-    if name == 'nt':
-        _ = system('cls')
- 
+    if name == "nt":
+        _ = system("cls")
+
     # Mac ou Linux
     else:
-        _ = system('clear')
-        
+        _ = system("clear")
 
 
-#Função 
+# Função
+
 
 def game():
-    
-	limpa_tela()     
-	print("\nBem-vindo(a) ao jogo dda forca!")
-	print("Adivinhe a palavra abaixo:\n")
+
+    limpa_tela()
+    print("\nBem-vindo(a) ao jogo dda forca!")
+    print("Adivinhe a palavra abaixo:\n")
+
 
 # Lista de palvras para o jogo
-palavras = ['banana', 'abacate', 'uva', 'morango', 'laranja']
+palavras = ["banana", "abacate", "uva", "morango", "laranja"]
 
 # Escolhe randomicamente uma palvra
-palvra = random.choice(palavras)
+palavra = random.choice(palavras)
 
 # List comprehensio
-letras_descobertas = ['_' for letra in palvra]
+letras_descobertas = ["_" for letra in palavra]
 
 # Número de chances
 chances = 6
@@ -40,8 +42,24 @@ chances = 6
 # Lista para as letras erradas
 letras_erradas = []
 
-#teste de update no github!!!
-print("teste de github")
+# Loop enquanto número de chances for maior que zero
+while chances > 0:
 
+    # print
+    print(" ".join(letras_descobertas))
+    print("\nChances restantes:", chances)
+    print("Letras erradas:", " ".join(letras_erradas))
 
+    # Tentativa
+    tentativa = input("\nDigite uma letra: ").lower()
 
+    # Condicional
+    if tentativa in palavra:
+        index = 0
+        for letra in palavra:
+            if tentativa == letra:
+                letras_descobertas[index] = letra
+            index += 1
+    else:
+        chances -= 1
+        letras_erradas.append(tentativa)
