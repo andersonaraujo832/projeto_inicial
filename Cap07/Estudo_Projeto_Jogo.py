a52c2e68c8b17976f9ae39c3638f0d199f237b5d
Anderson Araujo <aoliveira832@gamil.com>
2025-08-18 14:57:52 -0300

    atualizando projetos

--- Cap07/Estudo_Projeto_Jogo.py
+++ Cap07/Estudo_Projeto_Jogo.py
@@ -26,40 +26,54 @@ def game():
     print("\nBem-vindo(a) ao jogo dda forca!")
     print("Adivinhe a palavra abaixo:\n")
 
+    # Lista de palvras para o jogo
+    palavras = ["banana", "abacate", "uva", "morango", "laranja"]
 
-# Lista de palvras para o jogo
-palavras = ["banana", "abacate", "uva", "morango", "laranja"]
+    # Escolhe randomicamente uma palvra
+    palavra = random.choice(palavras)
 
-# Escolhe randomicamente uma palvra
-palavra = random.choice(palavras)
+    # List comprehensio
+    letras_descobertas = ["_" for letra in palavra]
 
-# List comprehensio
-letras_descobertas = ["_" for letra in palavra]
+    # Número de chances
+    chances = 6
 
-# Número de chances
-chances = 6
+    # Lista para as letras erradas
+    letras_erradas = []
 
-# Lista para as letras erradas
-letras_erradas = []
+    # Loop enquanto número de chances for maior que zero
+    while chances > 0:
 
-# Loop enquanto número de chances for maior que zero
-while chances > 0:
+        # print
+        print(" ".join(letras_descobertas))
+        print("\nChances restantes:", chances)
+        print("Letras erradas:", " ".join(letras_erradas))
 
-    # print
-    print(" ".join(letras_descobertas))
-    print("\nChances restantes:", chances)
-    print("Letras erradas:", " ".join(letras_erradas))
+        # Tentativa
+        tentativa = input("\nDigite uma letra: ").lower()
 
-    # Tentativa
-    tentativa = input("\nDigite uma letra: ").lower()
+        # Condicional
+        if tentativa in palavra:
+            index = 0
+            for letra in palavra:
+                if tentativa == letra:
+                    letras_descobertas[index] = letra
+                index += 1
+        else:
+            chances -= 1
+            letras_erradas.append(tentativa)
+
+        # Condicional
+        if "_" not in letras_descobertas:
+            print("\nVocê venceu, a palvra era:", palavra)
+            break
 
     # Condicional
-    if tentativa in palavra:
-        index = 0
-        for letra in palavra:
-            if tentativa == letra:
-                letras_descobertas[index] = letra
-            index += 1
-    else:
-        chances -= 1
-        letras_erradas.append(tentativa)
+    if "_" in letras_descobertas:
+        print("\nVocê perdeu, a palavra era:", palavra)
+
+
+# Bloco main
+if __name__ == "__main__":
+    game()
+    print("\nParabéns. Você está aprendendo programação em Python com a DSA. :)\n")
